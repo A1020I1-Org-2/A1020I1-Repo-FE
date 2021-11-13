881f86e72d82216f50fd24834c36e4972162c500
thanghaqn2000 <57361234+thanghaqn2000@users.noreply.github.com>
2021-11-13 13:34:29 +0700

    update FE

--- pawn-system-management/src/app/app-routing.module.ts
+++ pawn-system-management/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {StatisticRoutingModule} from "./statistic/statistic-routing.module";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule,StatisticRoutingModule,
+  HttpClientModule]
 })
 export class AppRoutingModule { }
--- pawn-system-management/src/app/app.module.ts
+++ pawn-system-management/src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {DatePipe} from "@angular/common";
+import {StatisticModule} from "./statistic/statistic.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StatisticModule
   ],
   providers: [],
   bootstrap: [AppComponent]
