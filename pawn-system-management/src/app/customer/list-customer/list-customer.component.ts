6037f81b178339645a968b10479904c8273d0a05
chuotqb <thanhdat.dut@gmail.com>
2021-11-23 23:06:35 +0700

    update

--- pawn-system-management/src/app/customer/list-customer/list-customer.component.ts
+++ pawn-system-management/src/app/customer/list-customer/list-customer.component.ts
@@ -4,6 +4,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { Customer } from 'src/app/interface/customer';
 import { CustomerService } from 'src/app/services/customer.service';
 import { CreateCustomerComponent } from '../create-customer/create-customer.component';
+import { ValidateCustomer } from './customer-validate';
 
 @Component({
   selector: 'app-list-customer',
@@ -16,6 +17,7 @@ export class ListCustomerComponent implements OnInit {
   idCustomerEdit: any;
   customerCurrent!: Customer;
   emptyObject = this.customerCurrent == null ? true : false;
+  customerValidate: ValidateCustomer = new ValidateCustomer(true, "", "", "", "", "", "", "")
   constructor(
     public formBuilder: FormBuilder,
     public customerService: CustomerService,
@@ -46,20 +48,34 @@ export class ListCustomerComponent implements OnInit {
   edit(customer: Customer) {
     this.customerCurrent = customer;
     this.customerCurrent.dateOfBirth = this.customerCurrent.dateOfBirth.substring(0, 10);
-    console.log(this.customerCurrent.dateOfBirth)
+    // console.log(this.customerCurrent.dateOfBirth)
     this.emptyObject = false;
     this.idCustomerEdit = customer.customerId;
-    console.log(this.emptyObject)
-    console.log(this.customerCurrent)
+    // console.log(this.emptyObject)
+    // console.log(this.customerCurrent)
+    this.customerValidate = new ValidateCustomer(true, this.customerCurrent.name, this.customerCurrent.dateOfBirth, this.customerCurrent.email, this.customerCurrent.address, this.customerCurrent.phone, this.customerCurrent.idCard, this.customerCurrent.img)
+    console.log(this.customerValidate)
   }
 
   editEmployee() {
-    this.customerService.update(this.customerCurrent).subscribe(data => {
-      console.log(data)
-      this.emptyObject = true;
-      this.getAllCustomer();
-    })
+    this.customerValidate.checkAge();
+    // console.log(this.customerValidate)
+    if (this.customerValidate.isValid) {
+      this.customerService.update({
+        customerId: this.customerCurrent.customerId,
+        name: this.customerValidate.name,
+        dateOfBirth: this.customerValidate.dateOfBirth,
+        email: this.customerValidate.email,
+        address: this.customerValidate.address,
+        phone: this.customerValidate.phone,
+        gender: this.customerCurrent.gender,
+        idCard: this.customerValidate.idCard,
+        img: this.customerValidate.img
+      }).subscribe(data => {
+        console.log(data)
+        this.emptyObject = true;
+        this.getAllCustomer();
+      })
+    }
   }
-
-
 }
