bc7117c977a56736bbb326058cb7ea1d1b792927
NHNJoker <ngonhanit@gmail.com>
2021-11-25 20:03:47 +0700

    nhannh2

--- pawn-system-management/src/app/contract/contract-routing.module.ts
+++ pawn-system-management/src/app/contract/contract-routing.module.ts
@@ -7,12 +7,14 @@ import {CommonModule} from "@angular/common";
 import {ListTop10ContractComponent} from "./list-top10-contract/list-top10-contract.component";
 import {UpdateContractComponent} from "./update-contract/update-contract.component";
 import {CreateLiquidationContractComponentComponent} from "./create-liquidation-contract-component/create-liquidation-contract-component.component";
+import {CreatePawnContractComponent} from "./create-pawn-contract/create-pawn-contract.component";
 
 const routes: Routes = [
   {path: 'listTop10Contract', component: ListTop10ContractComponent},
   {path: 'listContract',component: ListContractComponent},
   {path: 'editContract', component: UpdateContractComponent},
-  {path: 'create-liquidation-contract', component:CreateLiquidationContractComponentComponent}
+  {path: 'create-liquidation-contract', component:CreateLiquidationContractComponentComponent},
+  {path:'create-pawn',component:CreatePawnContractComponent}
   ]
 
 @NgModule({
